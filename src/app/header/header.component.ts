b5b00ef0bbfc46eccd3f4ea57955ecc626215554
KarenNgala <ngalalalu@gmail.com>
2020-06-22 02:31:33 +0300

    Move search button to users and increase font size

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,17 +6,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  searchTerm:string;
-  @Output() searchWord = new EventEmitter<any>();
-
   constructor() { }
 
   ngOnInit(): void {
   }
-  
-  getSearchItem(){
-    this.searchWord.emit(this.searchTerm);
-    this.searchTerm="";
-  }
-
 }
