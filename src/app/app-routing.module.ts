74f34c5eec56ce9c4e6d94f97e97a6fe1078b538
KarenNgala <ngalalalu@gmail.com>
2020-06-22 04:58:30 +0300

    Add routing functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './Error/not-found/not-found.component'
+import { UsersComponent } from './users/users.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:UsersComponent},
+  {path:'**',component:NotFoundComponent},
+  {path: '', redirectTo:"/home", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
